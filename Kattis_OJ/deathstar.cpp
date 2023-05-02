ea015a6b655c80676fcbe54bc4497e5e20cb4540
nguyen phuc loi <nguyenphucloi231@gmail.com>
2023-05-02 10:36:15 +0700

    Deathstar

---
+++ Kattis_OJ/deathstar.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int size;
+    cin >> size;
+
+    vector<int> plans;
+    plans.resize(size, 0);
+
+    for(int i = 0; i < size; i++) {
+        for(int j = 0; j < size; j++) {
+            int num;
+            cin >> num;
+            plans[i] |= num;
+        }
+        cout << plans[i] << " ";
+    }
+}
