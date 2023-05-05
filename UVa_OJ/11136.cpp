d131f7e3cc691dac98587893216f1a7ce2556913
nguyen phuc loi <nguyenphucloi231@gmail.com>
2023-05-05 18:57:30 +0700

    Hoax or what

---
+++ UVa_OJ/11136.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+	int n;
+	while (cin >> n && n) {
+		multiset<ll> s;
+		ll ans = 0;
+		for (int i = 1; i <= n; ++i) {
+			int t;
+			cin >> t;
+			for (int j = 1; j <= t; ++j) {
+				ll m;
+				cin >> m;
+				s.insert(m);
+			}
+			ans += *s.rbegin() - *s.begin();
+			s.erase(s.begin());
+			s.erase(--s.end());
+		}		
+		cout << ans << endl;
+	}
+	return 0;
+}
