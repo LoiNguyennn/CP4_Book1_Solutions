90057cb09f14dbdb4f9f840d494085f131b0168e
nguyen phuc loi <nguyenphucloi231@gmail.com>
2023-05-05 16:36:29 +0700

    Andy's First Dictionary

---
+++ UVa_OJ/10815.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool isAlphabet(char c) {
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
+
+int main() {
+	string in;
+	set<string> s;
+	while (cin >> in) {
+		for (int i = 0; i < in.length(); ++i) {
+			string tmp = "";
+			while (i < in.length() && isAlphabet(in[i])) {
+				if (in[i] >= 'A' && in[i] <= 'Z')
+					in[i] += 32;
+				tmp += in[i];
+				++i;
+			}
+			if (tmp != "")
+				s.insert(tmp);
+		}
+	}
+	for (auto x : s)
+		cout << x << endl;
+	return 0;
+}
