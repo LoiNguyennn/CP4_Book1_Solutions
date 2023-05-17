379453f683af6057abfa073017627a34cdc96235
nguyen phuc loi <nguyenphucloi231@gmail.com>
2023-05-17 14:15:46 +0700

    Hanoi Tower Troubles Again!

---
+++ UVa_OJ/10276.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int TC;
+	cin >> TC;
+	while (TC--) {
+		int n;
+		cin >> n;
+		int top[n];
+		memset(top, 0, sizeof(top));
+		int ans = 1;
+		while (true) {
+			int i;
+			for (i = 0; i < n; ++i) {
+				int tmp = top[i] + ans;
+				tmp = sqrt(tmp);
+				if (tmp * tmp == top[i] + ans || top[i] == 0) {
+					top[i] = ans;
+					break;
+				}
+			}
+			if (i == n) break;
+			++ans;
+		}
+		cout << ans - 1 << endl;
+	}
+	return 0;
+}
