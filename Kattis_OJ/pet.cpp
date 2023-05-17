f994278cb79525115c2d95a3d3491901762525e2
nguyen phuc loi <nguyenphucloi231@gmail.com>
2023-05-17 16:48:22 +0700

    Pet

---
+++ Kattis_OJ/pet.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int p[5];
+	int max_point = 0;
+	int winner = 0;
+	for (int i = 0; i < 5; ++i) {
+		int a, b, c, d;
+		cin >> a >> b >> c >> d;
+		p[i] = a + b + c + d;
+		if (p[i] > max_point) {
+			max_point = p[i];
+			winner = i;
+		}
+	}
+	cout << winner + 1 << " " << max_point << endl;
+	return 0;
+}
