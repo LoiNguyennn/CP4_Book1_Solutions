e9d6f1326f34576ecb71858adee381ec51aa884b
nguyen phuc loi <nguyenphucloi231@gmail.com>
2023-05-19 13:06:04 +0700

    Geppetto

---
+++ Kattis_OJ/geppetto.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+	set<pair<int, int>> s;
+	for (int i = 0; i < m; ++i) {
+		int a, b;
+		cin >> a >> b;
+		--a;
+		--b;
+		s.insert({a, b});
+	}
+
+	int ans = 0;
+
+	for (int mask = 0; mask < (1 << n); ++mask) {
+		bool ok = true;
+		for (auto x : s) {
+			int a = x.first, b = x.second;
+			if ((mask & (1 << a)) && (mask & (1 << b))) {
+				ok = false;
+				break;
+			}
+		}
+		if (ok)
+			++ans;
+	}
+	cout << ans << endl;
+	return 0;
+}
