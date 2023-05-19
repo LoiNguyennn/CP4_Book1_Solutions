f1868eccfc656ae851be5ef163ec1718631cea8f
nguyen phuc loi <nguyenphucloi231@gmail.com>
2023-05-19 22:36:43 +0700

    Jumbled Communication

---
+++ Kattis_OJ/communication.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	unordered_map<int, int> mp;
+	for (int i = 0; i < 256; ++i) {
+		int t = (i ^ (i << 1)) & 255;
+		mp[t] = i;
+	}
+
+	for (int i = 0; i < n; ++i) {
+		int a;
+		cin >> a;
+		cout << mp[a] << " ";
+	}
+	return 0;
+}
