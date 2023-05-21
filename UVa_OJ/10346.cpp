94703a00cce45212381f3fe89782380ec85579e0
nguyen phuc loi <nguyenphucloi231@gmail.com>
2023-05-21 15:42:37 +0700

    Peter's Smokes

---
+++ UVa_OJ/10346.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int n, k;
+	while (cin >> n >> k) {
+		int ans = n;
+		while (n >= k) {
+			n -= k - 1;
+			++ans;
+		}
+		cout << ans << endl;
+	}
+	return 0;
+}
