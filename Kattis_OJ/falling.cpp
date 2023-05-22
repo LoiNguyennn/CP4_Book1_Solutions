989c91820ebe9a67c4fc4a8856d5a65825f8f51a
nguyen phuc loi <nguyenphucloi231@gmail.com>
2023-05-22 09:07:18 +0700

    Falling Mugs

---
+++ Kattis_OJ/falling.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+#define EPSILON 1e-8
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+	ll D;
+	cin >> D;
+
+	for (ll a = sqrt(D); a <= (D + 1) / 2; ++a) {
+		double b = sqrt(a * a - D);
+		if (fabs(b - int(b)) <= EPSILON && b >= 0 && b <= (a - 1)) {
+			cout << b << " " << a << endl;
+			return 0;
+		}
+	}
+	cout << "impossible\n";
+	return 0;
+
+}
