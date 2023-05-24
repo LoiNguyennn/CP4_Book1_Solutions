6e616e77ddc0b170f7f708d3a7d108320a4d27a4
nguyen phuc loi <nguyenphucloi231@gmail.com>
2023-05-24 14:05:48 +0700

    Room Painting

---
+++ Kattis_OJ/roompainting.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+	ll n, m;
+	cin >> n >> m;
+	ll cans[n];
+	for (int i = 0; i < n; ++i)
+		cin >> cans[i];
+	sort(cans, cans + n);
+	ll ans = 0;
+	for (int i = 0; i < m; ++i) {
+		ll t;
+		cin >> t;
+		ans += *lower_bound(cans, cans + n, t) - t;
+	}
+	cout << ans << endl;
+	return 0;
+}
