40e1e6145b6be4306e8e20462122a125d56dfca6
nguyen phuc loi <nguyenphucloi231@gmail.com>
2023-06-11 10:51:54 +0700

    What is the Median?

---
+++ UVa_OJ/10107.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+ll a[10000];
+ll n;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	n = 0;
+	while (cin >> a[n]) {
+		++n;
+		sort(a, a + n);
+		if (n % 2 == 0) {
+			cout << (a[n / 2] + a[n / 2 - 1]) / 2 << endl; 
+		} else {
+			cout << a[n / 2] << endl;
+		}
+	}
+	return 0;
+}
