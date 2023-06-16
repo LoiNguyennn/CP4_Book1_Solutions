061d9a957018001626463db531f5626e4aaa27c5
nguyen phuc loi <nguyenphucloi231@gmail.com>
2023-06-16 07:48:36 +0700

    Age Sort

---
+++ UVa_OJ/11462.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int cnt[100];
+
+int main() {
+	int n;
+	while (scanf("%d", &n) && n) {
+		int t; 
+		for (int i = 0; i < n; ++i) {
+			scanf("%d", &t);
+			++cnt[t];
+		}
+		int flag = 0;
+		for (int i = 0; i < 100; ++i)
+			while (cnt[i] > 0) {
+				if (flag)
+					printf(" ");
+				flag = 1;
+				printf("%d", i);
+				--cnt[i];
+			}
+		printf("\n");
+		memset(cnt, 0, sizeof(cnt));
+	}
+	return 0;
+}
