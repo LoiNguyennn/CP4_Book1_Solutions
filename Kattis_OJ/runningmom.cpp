25933f59a62273ca33f0543ccdebed86dc373677
nguyen phuc loi <nguyenphucloi231@gmail.com>
2023-06-18 22:38:09 +0700

    Running MoM

---
+++ Kattis_OJ/runningmom.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+unordered_map<string, int> EXPLORED;
+unordered_map<string, vector<string>> AL;
+bool hasCycle;
+
+void dfs(string u) {
+	EXPLORED[u] = 1;
+	for (string v : AL[u]) {
+		if (EXPLORED[v] == 2) continue;
+		if (EXPLORED[v] == 1) {
+			hasCycle = true;
+			return;
+		}	
+		dfs(v);
+	}
+	EXPLORED[u] = 2;
+}
+
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; ++i) {
+		string o, d;
+		cin >> o >> d;
+		AL[o].push_back(d);
+	}
+	string s;
+	while (cin >> s) {
+		EXPLORED.clear();
+		hasCycle = false;
+		dfs(s);
+		cout << s << " ";
+		if (hasCycle) cout << "safe\n";
+		else cout << "trapped\n";
+	}
+	return 0;
+}
