cbb6cc76a79317cec4a5d580f30467a803400ce7
nguyen phuc loi <nguyenphucloi231@gmail.com>
2023-08-14 09:42:12 +0700

    Nuts and Bolts

---
+++ UVa_OJ/11138.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+
+using namespace std;
+
+int G[500][500];
+vector<int> match, visited;
+int bolts, nuts;
+
+
+int Aug(int L) {
+	if (visited[L]) return 0;
+	visited[L] = 1;
+	for (int R = 0; R < nuts; ++R) {
+		if (G[L][R])
+			if (match[R] == -1 || Aug(match[R])) {
+				match[R] = L;
+				return 1;
+			}
+	}
+	return 0;
+}
+
+int main() {
+	int TC;
+	cin >> TC;
+	for (int tc = 1; tc <= TC; ++tc) {
+		cin >> bolts >> nuts;
+		for (int i = 0; i < bolts; ++i) 
+			for (int j = 0; j < nuts; ++j) 
+				cin >> G[i][j];
+		
+		match.assign(nuts, -1);
+		int MCBM = 0;
+		for (int L = 0; L < bolts; ++L) {
+			visited.assign(bolts, 0);
+			MCBM += Aug(L);
+		}
+		cout << "Case " << tc << ": a maximum of " << MCBM << " nuts and bolts can be fitted together\n";
+	}		
+	return 0;
+}
