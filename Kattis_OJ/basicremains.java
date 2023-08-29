a136c03dd49c97e7d726a9e0276c5e7b9d06407f
nguyen phuc loi <nguyenphucloi231@gmail.com>
2023-08-29 10:02:28 +0700

    Basic Remains

---
+++ Kattis_OJ/basicremains.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            int b = scanner.nextInt();
+            if (b == 0) break;
+            BigInteger p = new BigInteger(scanner.next(), b);
+            BigInteger m = new BigInteger(scanner.next(), b);
+            System.out.println((p.mod(m)).toString(b));
+        }
+    }
+}
