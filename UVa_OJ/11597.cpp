e87b2017d93b3da1e0d2dfde7fbd00c008488f10
nguyen phuc loi <nguyenphucloi231@gmail.com>
2023-11-06 15:15:56 +0700

    Spanning Subtree

---
+++ UVa_OJ/11597.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    int TC = 1;
+    while (cin >> n && n) {
+        cout << "Case " << TC++ << ": " << (n >> 1) << endl;
+    }   
+    return 0;
+}
